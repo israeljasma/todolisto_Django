f733c21db832009c7eb8907a54901524193c5f38
Unknown <by.darklancer@gmail.com>
2018-05-16 20:25:11 -0400

    primer commit

---
+++ main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('home', views.index, name='home'),
+    path('', views.index, name='home'),
+    path('tareas', views.tareas, name='tareas'),
+    path('crear_tarea', views.crear_tarea, name='crear_tarea'),
+]
---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .models import Tarea
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required()
+def index(request):
+    return render(request, 'home.html')
+
+@login_required()
+def tareas(request):
+    tareas = Tarea.objects.filter(usuario=request.user)
+    # tareas = Tarea.objects.all()
+    return render(request, 'tareas.html', {'tareas' : tareas })
+
+@login_required()
+def crear_tarea(request):
+    if request.method == 'POST':
+        tarea = Tarea()
+        tarea.titulo = request.POST.get('titulo_tarea')
+        tarea.descripcion = request.POST.get('descripcion_tarea')
+        tarea.usuario = request.user
+        tarea.save()
+    return redirect('tareas')
