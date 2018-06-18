7bff6e54a13936d6cbeefb8e2c295c91600ea06d
Unknown <by.darklancer@gmail.com>
2018-06-18 07:50:44 -0400

    feat: form TareaUpdate

--- main/forms.py
+++ main/forms.py
@@ -1,3 +1,5 @@
+from .models import Tarea, TipoTarea, EstadoTarea
+from django  import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -15,4 +17,20 @@ class RegistroForm(UserCreationForm):
             'first_name': 'Nombre',
             'last_name': 'Apellido',
             'email': 'Email',
+        }
+
+class TareaUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Tarea
+        fields = [
+            'titulo',
+            'descripcion',
+            'fechaTermino',
+            'estadoTarea',
+        ]
+        labels = {
+            'titulo': 'Titulo',
+            'descripcion': 'Descripcion',
+            'fechaTermino': 'Fecha de termino',
+            'estadoTarea': 'Estado de la tarea',
         }
