948959b5bfb66aec28646c1be7f0855aa19df3c9
Unknown <by.darklancer@gmail.com>
2018-06-19 01:23:19 -0400

    feat: agregado tipo de tarea a la vista del administrador

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Tarea, EstadoTarea
+from .models import Tarea, EstadoTarea, TipoTarea
 
 # Register your models here.
 admin.site.register(Tarea)
 admin.site.register(EstadoTarea)
+admin.site.register(TipoTarea)
